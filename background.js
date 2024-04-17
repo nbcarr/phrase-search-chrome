0c0221765e2540a219c0542b1b622b2ad7d82347
Nick Carrolli <nickcarrolli@Nicks-MacBook-Pro.local>
2024-04-17 17:43:57 -0400

    fix bugs with adding new optional urls from popup menu

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
-const defaultSearches = [];
-defaultSearches.push({id: 'searchYoutube', title: "Search Youtube", url: "https://www.youtube.com/results?search_query="})
-defaultSearches.push({id: 'searchAmazon', title: "Search Amazon", url: "https://www.amazon.com/s?k="})
-defaultSearches.push({id: 'searchStackOverflow', title: "Search StackOverflow", url: "https://stackoverflow.com/search?q="})
-
+/**
+ * Returns a Promise representing all data in the sync storage
+ * @returns Promise
+ */
 function getDataFromSyncStorage() {
     return new Promise((resolve, reject) => {
         chrome.storage.sync.get(null, (data) => {
@@ -15,20 +14,29 @@ function getDataFromSyncStorage() {
     });
 }
 
-getDataFromSyncStorage().then((data) => {
-    let searches;
-    if (!data.customSearchList) {
-        searches = defaultSearches;
-    } else {
-      console.log(data.customSearchList)
-        const customSearches = [];
+/**
+ * Creates the list of search options in the context menu given the custom search options 
+ * @param {*} data Object representing all data in sync storage
+ */
+function createSearchContextMenu(data) {
+    let customSearches = [];
+    if (data.customSearchList) {
+        console.log(data.customSearchList)
         data.customSearchList.forEach(function (object) {
             customSearches.push({id: object.customInputSearchId, title: object.customInputSearchId, url: object.customInputSearchUrl})
         });
-        searches = defaultSearches.concat(customSearches);
+    }
+    const defaultSearches = [];
+    defaultSearches.push({id: 'searchYoutube', title: "Search Youtube", url: "https://www.youtube.com/results?search_query="})
+    defaultSearches.push({id: 'searchAmazon', title: "Search Amazon", url: "https://www.amazon.com/s?k="})
+    defaultSearches.push({id: 'searchStackOverflow', title: "Search StackOverflow", url: "https://stackoverflow.com/search?q="})
 
-    } searches.forEach(search => {
-        chrome.contextMenus.create({id: search.id, title: search.title, contexts: ["selection"]});
+    const searches = customSearches.length > 0 ? defaultSearches.concat(customSearches) : defaultSearches;
+
+    chrome.contextMenus.removeAll(() => {
+        searches.forEach(search => {
+            chrome.contextMenus.create({id: search.id, title: search.title, contexts: ["selection"]});
+        })
     })
 
     chrome.contextMenus.onClicked.addListener(function (info, tab) {
@@ -36,7 +44,27 @@ getDataFromSyncStorage().then((data) => {
             url: searches.find(search => search.id === info.menuItemId).url + encodeURIComponent(info.selectionText)
         })
     });
+}
 
-}).catch((error) => {
-    console.error("Error retrieving data from sync storage:", error);
+// On initial load
+getDataFromSyncStorage().then((data) => {
+    createSearchContextMenu(data)
+})
+
+// On new option added
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.msg === "option_added") {
+        getDataFromSyncStorage().then((data) => {
+            createSearchContextMenu(data)
+        })
+    }
+});
+
+// On options cleared
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.msg === "option_cleared") {
+        getDataFromSyncStorage().then((data) => {
+            createSearchContextMenu(data)
+        })
+    }
 });
--- popup.js
+++ popup.js
@@ -4,55 +4,60 @@ const saveOptions = async () => {
     const customInputSearchId = document.getElementById('customInputSearchId').value;
     const customInputSearchUrl = document.getElementById('customInputSearchUrl').value;
 
-    chrome.storage.sync.get({ customSearchList: [] }, function(data) {
-        // check for duplicates
+    chrome.storage.sync.get({
+        customSearchList: []
+    }, function (data) {
         let customSearchList = data.customSearchList;
-        const newCustomSearch = {customInputSearchId, customInputSearchUrl}
+        const newCustomSearch = {
+            customInputSearchId,
+            customInputSearchUrl
+        }
         customSearchList.push(newCustomSearch);
-        chrome.storage.sync.set({ customSearchList: customSearchList });
+        chrome.storage.sync.set({customSearchList: customSearchList});
 
         const status = document.getElementById('status');
         status.textContent = 'Options saved.';
         setTimeout(() => {
-          status.textContent = '';
-        }, 750);
+            status.textContent = '';
+        }, 1500);
+        displayOptions();
     });
 
-  };
+    chrome.runtime.sendMessage({msg: "option_added"});
 
-  const displayOptions = async () => {
+};
+
+const displayOptions = async () => {
     const savedInputSearches = document.getElementById('savedInputSearches');
     const currentOptions = await chrome.storage.sync.get(null);
-    if (!currentOptions.customSearchList) {
+    savedInputSearches.innerHTML = '';
+    if (! currentOptions.customSearchList) {
         let div = document.createElement('div');
-        
-        // Set the content of the div using object properties
         div.textContent = `No options set`;
-    
-        // Append the div to the parent element
         savedInputSearches.appendChild(div);
         return;
     }
-    currentOptions.customSearchList.forEach(function(object) {
-        // Create a new div element
+    currentOptions.customSearchList.forEach(function (object) {
         let div = document.createElement('div');
-        
-        // Set the content of the div using object properties
-        div.textContent = `Search ID: ${object.customInputSearchId}, Search URL: ${object.customInputSearchUrl}`;
-    
-        // Append the div to the parent element
+        div.textContent = `Search ID: ${
+            object.customInputSearchId
+        }, Search URL: ${
+            object.customInputSearchUrl
+        }`;
         savedInputSearches.appendChild(div);
-    }); 
-  }
+    });
+}
 
-  const clearOptions = () => {
+const clearOptions = () => {
     chrome.storage.sync.clear()
     const savedInputSearches = document.getElementById('savedInputSearches');
     savedInputSearches.innerHTML = '';
-    displayOptions();
-  }
-  
-  document.addEventListener('DOMContentLoaded', displayOptions);
-  document.getElementById('save').addEventListener('click', saveOptions);
-  document.getElementById('clear').addEventListener('click', clearOptions);
 
+    chrome.runtime.sendMessage({msg: "option_cleared"});
+
+    displayOptions();
+}
+
+document.addEventListener('DOMContentLoaded', displayOptions);
+document.getElementById('save').addEventListener('click', saveOptions);
+document.getElementById('clear').addEventListener('click', clearOptions);
